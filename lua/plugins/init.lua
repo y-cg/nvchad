be5a502f68d4d6866d200d27152e2114cfffb45e
y-cg <159802291+y-cg@users.noreply.github.com>
2026-01-18 02:56:59 +0800

    feat(telescope): show hidden files in find_files (except .git)

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -46,6 +46,19 @@ return {
   },
 
   {
+    "nvim-telescope/telescope.nvim",
+    opts = function()
+      local opts = require "nvchad.configs.telescope"
+      opts.pickers = opts.pickers or {}
+      opts.pickers.find_files = vim.tbl_deep_extend("force", opts.pickers.find_files or {}, {
+        hidden = true,
+        find_command = { "rg", "--files", "--hidden", "--glob", "!.git/*" },
+      })
+      return opts
+    end,
+  },
+
+  {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       ensure_installed = {
